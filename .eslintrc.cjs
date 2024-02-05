a4252a45f77bbb9f585095fb6e53789cd9697039
Drew Hoover <drew@greatexpectations.io>
2024-02-05 14:26:03 -0500

    add linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,32 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:react-hooks/recommended",
+    "plugin:storybook/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigRootDir: __dirname,
+  },
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
--- src/controls/UnknownControl.tsx
+++ src/controls/UnknownControl.tsx
@@ -4,8 +4,8 @@ export function UnknownControl(props: ControlProps | CellProps) {
   // console.log({ schema: props.schema, path: props.path, schemaPath: props.schemaPath, props })
   return (
     <div style={{ color: "red" }}>
-      No applicable renderer found for {props.schema?.type} type field "
-      {props.path}".
+      No applicable renderer found for {props.schema?.type} type field &quot;
+      {props.path}&quot;.
     </div>
   );
 }

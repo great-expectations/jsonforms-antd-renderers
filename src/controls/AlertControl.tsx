de8e35a7eaac900e7652028b7c3b9a831cf7cf3b
Nathan Farmer <nathan@greatexpectations.io>
2024-02-28 15:01:25 -0500

    Use the AlertLabelOptions from AlertProps and remove tests of antd behavior

--- src/controls/AlertControl.tsx
+++ src/controls/AlertControl.tsx
@@ -1,11 +1,9 @@
 import { LabelProps, RendererProps } from "@jsonforms/core"
-import { Alert } from "antd"
+import { Alert, AlertProps } from "antd"
 
-export type AlertLabelOptions = {
-    type?: "warning" | "success" | "info"
-  }
+export type AlertLabelOptions = Pick<AlertProps, "type">
 
 export function AlertControl({ text, uischema }: LabelProps & RendererProps) {
-  const options: AlertLabelOptions = uischema?.options as AlertLabelOptions
+  const options = uischema?.options as AlertLabelOptions
   return <Alert style={{ marginBottom: "24px" }} type={options?.type} message={text} showIcon />
 }

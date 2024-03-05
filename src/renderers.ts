1186933d6e578437739bbd0fda868ee49fa06cbd
Nathan Farmer <NathanFarmer@users.noreply.github.com>
2024-03-05 14:43:03 -0500

    feat: Add `NumberControl` (#16)
    * Port over existing code
    
    * Fix typing errors
    
    * Exclude .ts files from prodution build
    
    * Exclude all files with .test. from prod build
    
    * Add untracked file
    
    * Remove unused file
    
    * Move tests to another branch
    
    * Push trivial change
    
    * Revert omit change
    
    * Remove NumericControlProps and use cast
    
    * Add existing tests
    
    * Revert "Add existing tests"
    
    This reverts commit 3c671dd340b34e51ddb035847193df6b8d406cea.
    
    * Update to new registry pattern
    
    * Pass min, max, step, and controls to InputNumber also
    
    * Only show percentage addonAfter if isPercentage
    
    * Only show percentage addonAfter if isPercentage
    
    * Fix marginLeft on pure number slider
    
    * Update tooltip logic for non-percentages on slider
    
    * Update logic to fill functionality gaps
    
    * Add logic for required asterisk
    
    * Fix tooltips
    
    * Update value default logic
    
    * Fix more broken empty value logic

--- src/renderers.ts
+++ src/renderers.ts
@@ -1,4 +1,4 @@
-import { JsonFormsRendererRegistryEntry,JsonFormsCellRendererRegistryEntry, isBooleanControl, isStringControl, rankWith, uiTypeIs } from "@jsonforms/core";
+import { JsonFormsRendererRegistryEntry,JsonFormsCellRendererRegistryEntry, isBooleanControl, isNumberControl, isStringControl, rankWith, uiTypeIs } from "@jsonforms/core";
 import { withJsonFormsControlProps, withJsonFormsLabelProps, withJsonFormsCellProps, withJsonFormsLayoutProps } from "@jsonforms/react";
 
 import { BooleanControl } from "./controls/BooleanControl";
@@ -6,15 +6,17 @@ import { AlertControl } from "./controls/AlertControl";
 import { TextControl } from "./controls/TextControl";
 import { UnknownControl } from "./controls/UnknownControl";
 import { VerticalLayoutRenderer } from "./layouts/VerticalLayout";
+import { NumberControl } from "./controls/NumberControl";
 
 
 // Ordered from lowest rank to highest rank. Higher rank renderers will be preferred over lower rank renderers.
 export const rendererRegistryEntries: JsonFormsRendererRegistryEntry[] = [
-  { tester: rankWith(1, () => true), renderer: withJsonFormsControlProps(UnknownControl)},
-  { tester: rankWith(2, uiTypeIs("VerticalLayout")), renderer: withJsonFormsLayoutProps(VerticalLayoutRenderer)},
+  { tester: rankWith(1, () => true), renderer: withJsonFormsControlProps(UnknownControl) },
+  { tester: rankWith(2, uiTypeIs("VerticalLayout")), renderer: withJsonFormsLayoutProps(VerticalLayoutRenderer) },
   { tester: rankWith(2, isBooleanControl), renderer: withJsonFormsControlProps(BooleanControl) },
-  { tester: rankWith(2, isStringControl), renderer: withJsonFormsControlProps(TextControl)},
-  { tester: rankWith(100, uiTypeIs("Label")),renderer: withJsonFormsLabelProps(AlertControl)},
+  { tester: rankWith(2, isStringControl), renderer: withJsonFormsControlProps(TextControl) },
+  { tester: rankWith(2, uiTypeIs("Label")), renderer: withJsonFormsLabelProps(AlertControl) },
+  { tester: rankWith(2, isNumberControl), renderer: withJsonFormsControlProps(NumberControl) },
 ];
 
 export const cellRegistryEntries: JsonFormsCellRendererRegistryEntry[] = [

fbece4d642f59f4e8ef8bbf9a1e58cdbf6674a1e
Thu Pham <thu.pham@greatexpectations.io>
2024-03-08 09:35:30 -0800

    More WIP and debugging

--- src/controls/DatetimeControl.test.tsx
+++ src/controls/DatetimeControl.test.tsx
@@ -3,7 +3,24 @@ import { render } from "../common/test-render"
 
 import { screen } from "@testing-library/react"
 import { datetimeSchema, datetimeUISchema, datetimeUISchemaWithRule } from "../testSchemas/datetimeSchema"
+import { isDateTimeControl, rankWith } from "@jsonforms/core"
 
+describe("DatetimeControlTester", () => {
+  test("tester works", () => {
+    const uiSchema = {
+      type: "Control",
+      scope: "#/properties/datetime",
+    }
+    const schema = {
+      type: "string",
+      title: "The Future is Now",
+      format: "date-time",
+    }
+    const context = {rootSchema: datetimeSchema, config: {}}
+
+    expect(rankWith(1, isDateTimeControl)(uiSchema, schema, context)).toBe(1)
+  })
+})
 describe("DatetimeControl", () => {
   const timestamp = "2023-07-18T01:02:01.182Z"
   const title = "The Future is Now"
--- src/stories/controls/DatetimeControl.stories.tsx
+++ src/stories/controls/DatetimeControl.stories.tsx
@@ -3,6 +3,7 @@ import { cellRegistryEntries, rendererRegistryEntries } from "../../renderers";
 import { StorybookAntDJsonForm } from "../../common/StorybookAntDJsonForm";
 import { datetimeSchema, datetimeUISchema } from "../../testSchemas/datetimeSchema";
 import { JsonForms } from "@jsonforms/react";
+import { DatetimeControl } from "../../controls/DatetimeControl";
 
 const meta: Meta<typeof StorybookAntDJsonForm> = {
   title: "Control/Datetime",
@@ -15,10 +16,14 @@ const meta: Meta<typeof StorybookAntDJsonForm> = {
     ]
   },
   argTypes: {
-    rendererRegistryEntries: {},
+    rendererRegistryEntries: { table: { disable: true } },
+    uiSchemaRegistryEntries: { table: { disable: true } },
     jsonSchema: {
       control: "object",
-    }
+    },
+    data: {table: {disable: true}}, 
+    config: {control: "object"},
+    onChange: {table: {disable: true, action: "on-change"}},
   }
 };
 
@@ -33,13 +38,41 @@ export const Datetime: Story = {
   },
 }
 
-export const DateTimeControl: Story = {
+export const DateTimeControlUsingJsonForms: Story = {
   render: () => (
     <JsonForms
       data={{datetime: "2023-07-18T01:02:01.182Z"}}
       schema={datetimeSchema}
       cells={[...cellRegistryEntries]}
       renderers={[...rendererRegistryEntries]}
+    />
+  ),
+}
+
+export const DateTimeControlComponent: Story = {
+  argTypes: {
+    rendererRegistryEntries: { table: { disable: true } },
+    uiSchemaRegistryEntries: { table: { disable: true } },
+    jsonSchema: {
+      control: "object",
+    },
+    data: {table: {disable: true}}, 
+    config: {control: "object"},
+    onChange: {table: {disable: true, action: "on-change"}},
+  },
+  render: () => (
+    <DatetimeControl
+      data="2023-07-18T01:02:01.182Z"
+      path="datetime"
+      schema={datetimeSchema}
+      handleChange={(result) => console.log(result)}
+      label="Datetime"
+      uischema={datetimeUISchema["elements"][0]}
+      errors=""
+      rootSchema={{}}
+      id=""
+      enabled={true}
+      visible={true}
     />
   ),
 }

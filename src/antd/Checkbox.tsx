f0015c0d9214b134ea1f8b5c88b7488459becdd6
Thu Pham <thu.pham@greatexpectations.io>
2024-03-11 11:45:43 -0700

    remove additional parens

--- src/antd/Checkbox.tsx
+++ src/antd/Checkbox.tsx
@@ -1,26 +1,38 @@
-import { CellProps, WithClassname, Helpers } from "@jsonforms/core"
-import { Checkbox as AntDCheckbox } from "antd"
-import { CheckboxChangeEvent } from "antd/es/checkbox"
+import { CellProps, WithClassname, Helpers } from "@jsonforms/core";
+import { Checkbox as AntDCheckbox } from "antd";
+import { CheckboxChangeEvent } from "antd/es/checkbox";
 
 interface CheckboxProps extends CellProps, WithClassname {
-  label?: string
+  label?: string;
 }
 
-export function Checkbox({ data, className, id, enabled, uischema, schema, path, handleChange, label } : CheckboxProps) {
-  const checked = !!data // convert undefined to false
+export function Checkbox({
+  data,
+  className,
+  id,
+  enabled,
+  uischema,
+  schema,
+  path,
+  handleChange,
+  label,
+}: CheckboxProps) {
+  const checked = !!data; // convert undefined to false
 
   const labelDescription = Helpers.createLabelDescriptionFrom(uischema, schema);
-  const defaultLabel = labelDescription.show ? labelDescription.text : '';
+  const defaultLabel = labelDescription.show ? labelDescription.text : "";
 
   return (
     <AntDCheckbox
       checked={checked}
-      onChange={(e: CheckboxChangeEvent) => {handleChange(path, e.target.checked)}}
+      onChange={(e: CheckboxChangeEvent) =>
+        handleChange(path, e.target.checked)
+      }
       className={className}
       id={id}
       disabled={!enabled}
     >
       {label ?? defaultLabel}
     </AntDCheckbox>
-  )
+  );
 }

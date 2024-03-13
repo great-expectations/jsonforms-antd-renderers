f47f8415a9313028cbd7ce0fdaf8eec88bd828fa
Nathan Farmer <nathan@greatexpectations.io>
2024-03-13 11:47:49 -0400

    Import types

--- src/controls/NumericControls/NumericControl.test.tsx
+++ src/controls/NumericControls/NumericControl.test.tsx
@@ -14,6 +14,7 @@ import {
   numericPriceSchema,
   numericUSDUISchema,
 } from "../../testSchemas/numericSchema/numericSchema"
+import "@testing-library/jest-dom"
 
 
 describe("NumericControl", () => {

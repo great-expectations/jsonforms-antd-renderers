827f6681da6f20de65154518318283464351dc65
Nathan Farmer <nathan@greatexpectations.io>
2024-03-14 13:41:56 -0400

    Add basing rendering tests for each layout

---
+++ src/layouts/VerticalLayoutRenderer.test.tsx
@@ -0,0 +1,16 @@
+import { test } from "vitest"
+import { screen } from "@testing-library/react"
+import { render } from "../common/test-render"
+
+import {
+  numericMagnitudeSchema,
+  numericVerticalUISchema,
+} from "../testSchemas/numericSchema/numericSchema"
+
+test("Vertical layout renders", async () => {
+  render({
+    schema: numericMagnitudeSchema,
+    uischema: numericVerticalUISchema,
+  })
+  await screen.findByRole("spinbutton")
+})

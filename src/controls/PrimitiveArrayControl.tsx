b46b3431ba3c519d9ce2c31653e137de5b67313e
Nathan Farmer <nathan@greatexpectations.io>
2024-03-27 14:28:17 -0400

    Add default record to PrimitiveArrayControl

--- src/controls/PrimitiveArrayControl.tsx
+++ src/controls/PrimitiveArrayControl.tsx
@@ -9,12 +9,15 @@ import {
   JsonFormsDispatch,
   withJsonFormsArrayControlProps,
 } from "@jsonforms/react"
-import { Form, Button } from "antd"
+import { Form, Button, Col, Row } from "antd"
 import type { Rule } from "antd/es/form"
-import { useCallback, useMemo } from "react"
+import { useEffect, useMemo } from "react"
 import { ArrayControlOptions } from "../ui-schema"
+import { usePreviousValue } from "../common/usePreviousValue"
+import React from "react"
 
 function PrimitiveArrayControl({
+  data,
   enabled,
   path,
   schema,
@@ -42,10 +45,17 @@ function PrimitiveArrayControl({
     [uischemas, schema, path, uischema, rootSchema],
   )
 
-  const innerCreateDefaultValue = useCallback(
-    () => createDefaultValue(schema, rootSchema) as unknown,
-    [schema, rootSchema],
+  const addItemToList = useMemo(
+    () => addItem(path, createDefaultValue(schema, rootSchema)),
+    [addItem, path, schema, rootSchema],
   )
+
+  const prevDataValue = usePreviousValue(data)
+  useEffect(() => {
+    if (data === undefined && prevDataValue === null) {
+      addItemToList()
+    }
+  })
 
   if (!visible) {
     return null
@@ -63,14 +73,14 @@ function PrimitiveArrayControl({
       xs: { span: 24 },
       sm: { span: 20 },
     },
-  };
+  }
   
   const formItemLayoutWithOutLabel = {
     wrapperCol: {
       xs: { span: 24, offset: 0 },
       sm: { span: 20, offset: 4 },
     },
-  };
+  }
 
   const options: ArrayControlOptions =
     (uischema.options as ArrayControlOptions) ?? {}
@@ -81,56 +91,75 @@ function PrimitiveArrayControl({
 
   return (
     <Form.List name="names">
-      {(fields, { add, remove }, { errors }) => (
+      {(fields, { add, remove }, { errors }) => {
+        fields.length === 0 && add()
+        return (
         <>
           {fields.map((field, index) => (
             <Form.Item
               {...(index === 0 ? formItemLayout : formItemLayoutWithOutLabel)}
               label={index === 0 ? label : ""}
-              required={false}
+              required={required}
               key={index}
               rules={rules}
             >
-              <JsonFormsDispatch
-                enabled={enabled}
-                schema={schema}
-                path={composePaths(path, `${index}`)}
-                uischema={foundUISchema}
-                renderers={renderers}
-                cells={cells}
-                uischemas={uischemas}
-              />
-              {fields.length > 1 ? (
-                <Button
-                  {...options.removeButtonProps}
-                  onClick={(e) => {
-                    e.stopPropagation()
-                    remove(field.name)
-                    removeItems?.(path, [index])()
-                  }}
-                />
-              ) : null}
+              <Row gutter={12}>
+                <Col>
+                  <JsonFormsDispatch
+                    enabled={enabled}
+                    schema={schema}
+                    path={composePaths(path, `${index}`)}
+                    uischema={foundUISchema}
+                    renderers={renderers}
+                    cells={cells}
+                    uischemas={uischemas}
+                  />
+                </Col>
+                <Col>
+                  {fields.length > 1 ? (
+                    <Button
+                      key="remove"
+                      disabled={
+                        !removeItems ||
+                        (required && fields.length === 1 && index === 0)
+                      }
+                      {...options.removeButtonProps}
+                      onClick={(e) => {
+                        e.stopPropagation()
+                        remove(field.name)
+                        removeItems?.(path, [index])()
+                      }}
+                    >
+                      {options.removeButtonProps?.children ?? "Delete"}
+                    </Button>
+                  ) : null}
+                </Col>
+              </Row>
             </Form.Item>
           ))}
-          <Form.Item>
-            <Button
-              {...options.addButtonProps}
-              onClick={(e) => {
-                e.stopPropagation()
-                add()
-                addItem(path, innerCreateDefaultValue())()
-              }}
-            >
-              {options.addButtonProps?.children ?? `Add ${label}`}
-            </Button>
+          <Form.Item
+            {...formItemLayoutWithOutLabel}
+          >
+            <Row>
+              <Button
+                {...options.addButtonProps}
+                onClick={(e) => {
+                  e.stopPropagation()
+                  add()
+                  addItemToList()
+                }}
+              >
+                {options.addButtonProps?.children ?? `Add ${label}`}
+              </Button>
+            </Row>
             <Form.ErrorList errors={errors} />
           </Form.Item>
         </>
-      )}
+      )}}
     </Form.List>
   )
 }
 
 export const PrimitiveArrayRenderer = withJsonFormsArrayControlProps(
-  PrimitiveArrayControl,
+  React.memo(PrimitiveArrayControl),
 )

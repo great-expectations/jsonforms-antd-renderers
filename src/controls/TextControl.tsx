07011638b7de85e8e827e76c5129a2275f806c6a
Nathan Farmer <nathan@greatexpectations.io>
2024-05-20 12:14:34 -0400

    Cleanup

--- src/controls/TextControl.tsx
+++ src/controls/TextControl.tsx
@@ -63,6 +63,7 @@ export function TextControl({
       rules={rules}
       validateTrigger={["onBlur"]}
       tooltip={tooltip}
+      {...formItemProps}
     >
       <TextControlInput
         aria-label={ariaLabel}

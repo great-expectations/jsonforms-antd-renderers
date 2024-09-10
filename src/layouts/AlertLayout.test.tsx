b89c3084a9d032dd20038c3fb1f21ec000c698aa
Aaron Ross <aaron.ross@greatexpectations.io>
2024-09-10 13:42:21 -0700

    use react-markdown in alerts to render markdown content as html

--- src/layouts/AlertLayout.test.tsx
+++ src/layouts/AlertLayout.test.tsx
@@ -1,4 +1,4 @@
-import { test } from "vitest"
+import { test, expect } from "vitest"
 import { screen } from "@testing-library/react"
 import { render } from "../common/test-render"
 import { JSONSchema } from "json-schema-to-ts"
@@ -32,4 +32,56 @@ test("AlertLayout renders", async () => {
     },
   })
   await screen.findByRole("alert")
+})
+
+test("AlertLayout renders markdown content", async () => {
+  render({
+    schema,
+    uischema: {
+      type: "VerticalLayout",
+      elements: [
+        {
+          type: "Label",
+          text: `# Title 1
+
+## Title 2
+
+### Title 3
+
+#### Title 4
+
+##### Title 5
+
+###### Title 6
+
+[Link Text](https://example.com "Link Title")
+
+\`Code Block\`
+
+- ul1
+- ul2
+- ul3
+
+1. ol1
+2. ol2
+
+just regular text
+`,
+        },
+      ],
+    },
+  })
+
+  await screen.findByRole("alert")
+  expect(screen.getByText("Link Text")).toHaveAttribute(
+    "href",
+    "https://example.com",
+  )
+  const link = screen.getByText("Link Text")
+  expect(link).toHaveAttribute("title", "Link Title")
+  expect(link).toHaveAttribute("target", "_blank")
+  expect(link).toHaveAttribute("rel", "noreferrer")
+  expect(link).toHaveAttribute("href", "https://example.com")
+  expect(link.tagName).toBe("A")
+  expect(screen.getByText("just regular text").tagName).toBe("SPAN")
 })
--- src/layouts/AlertLayout.tsx
+++ src/layouts/AlertLayout.tsx
@@ -2,15 +2,38 @@ import { LabelProps, RendererProps } from "@jsonforms/core"
 import { Alert } from "antd"
 import { AlertLayoutOptions } from "../ui-schema"
 import { withJsonFormsLabelProps } from "@jsonforms/react"
+import ReactMarkdown from "react-markdown"
 
 export function AlertLayout({ text, uischema }: LabelProps & RendererProps) {
   const options = uischema.options as AlertLayoutOptions
   return (
     <Alert
-      style={{ marginBottom: "24px" }}
-      type={options?.type}
-      message={text}
-      showIcon
+      style={{ marginBottom: "24px", borderWidth: 0, padding: "12px" }}
+      type={options?.type ?? "info"}
+      description={
+        <ReactMarkdown
+          components={{
+            p(props) {
+              return <span {...props} />
+            },
+            a(props) {
+              return (
+                <a
+                  style={{ textDecoration: "underline", color: "inherit" }}
+                  href={props.href}
+                  title={props.title}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {props.children}
+                </a>
+              )
+            },
+          }}
+        >
+          {text ?? ""}
+        </ReactMarkdown>
+      }
     />
   )
 }

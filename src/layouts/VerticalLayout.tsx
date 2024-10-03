852b35b70ad73f02e3021f8cbcb7f7060cf92b04
Aaron Ross <aaron.ross@greatexpectations.io>
2024-10-03 12:19:15 -0700

    remove props on every render, remove div breaking control flow

--- src/layouts/VerticalLayout.tsx
+++ src/layouts/VerticalLayout.tsx
@@ -1,6 +1,6 @@
 import { LayoutProps, GroupLayout } from "@jsonforms/core"
 import { AntDLayout, AntDLayoutProps } from "./LayoutRenderer"
-import { Form, FormInstance, FormProps } from "antd"
+import { Form } from "antd"
 import { VerticalLayoutUISchema } from "../ui-schema"
 import { withJsonFormsLayoutProps } from "@jsonforms/react"
 
@@ -24,20 +24,13 @@ export function VerticalLayout({
   }
   const form = Form.useFormInstance()
   return (
-    <Form {...getFormLayoutOptions(form)} form={form}>
-      {!groupLayout.label && (
+    <Form component={form ? false : "form"} scrollToFirstError form={form}>
+      {!!groupLayout.label && (
         <div>{groupLayout.label}</div> // this was SubtitleSemiBold
       )}
       <AntDLayout {...childProps} renderers={renderers} cells={cells} />
     </Form>
   )
-}
-
-function getFormLayoutOptions(form: FormInstance): Partial<FormProps> {
-  return {
-    scrollToFirstError: true,
-    component: form ? "div" : "form",
-  }
 }
 
 export const VerticalLayoutRenderer = withJsonFormsLayoutProps(VerticalLayout)

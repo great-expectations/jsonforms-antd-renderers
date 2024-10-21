33ff83205f1805006e35762dd2d318cb21685eb7
Aaron Ross <aaron.ross@greatexpectations.io>
2024-10-21 14:13:16 -0700

    feature(DateTimeControl): add control, tests and stories

---
+++ src/stories/controls/DateTimeControl.stories.tsx
@@ -0,0 +1,65 @@
+import { Meta, StoryObj } from "@storybook/react"
+import { StorybookAntDJsonForm } from "../../common/StorybookAntDJsonForm"
+import {
+  dateTimeSchema,
+  dateTimeShowTimeUISchema,
+  dateTimeUISchema,
+  dateTimeOverrideDefaultFormatUISchema,
+  dateTimeShowMillisecondHideNowUISchema,
+  dateTimeDefaultValueSchema,
+} from "../../testSchemas/dateTimeSchema"
+
+const meta: Meta<typeof StorybookAntDJsonForm> = {
+  title: "Control/DateTime",
+  component: StorybookAntDJsonForm,
+  tags: ["autodocs"],
+  args: {
+    jsonSchema: dateTimeSchema,
+    uiSchema: dateTimeUISchema,
+  },
+  argTypes: {},
+}
+
+export default meta
+
+type Story = StoryObj<typeof StorybookAntDJsonForm>
+
+export const RequiredDatetime: Story = {
+  tags: ["autodocs"],
+  args: {
+    jsonSchema: dateTimeSchema,
+    uiSchema: dateTimeUISchema,
+  },
+}
+
+export const ShowTime: Story = {
+  tags: ["autodocs"],
+  args: {
+    jsonSchema: dateTimeSchema,
+    uiSchema: dateTimeShowTimeUISchema,
+  },
+}
+
+export const ShowMillisecondHideNow: Story = {
+  tags: ["autodocs"],
+  args: {
+    jsonSchema: dateTimeSchema,
+    uiSchema: dateTimeShowMillisecondHideNowUISchema,
+  },
+}
+
+export const OverrideDefaultFormat: Story = {
+  tags: ["autodocs"],
+  args: {
+    jsonSchema: dateTimeSchema,
+    uiSchema: dateTimeOverrideDefaultFormatUISchema,
+  },
+}
+
+export const DefaultValue: Story = {
+  tags: ["autodocs"],
+  args: {
+    jsonSchema: dateTimeDefaultValueSchema,
+    uiSchema: dateTimeUISchema,
+  },
+}
